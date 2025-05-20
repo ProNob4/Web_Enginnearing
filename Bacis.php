4feea285577c7e84b33b34eb3d04c490d48a1c6f
ProNob <112222527+ProNob4@users.noreply.github.com>
2025-05-20 16:31:15 +0600

    Create Bacis.php

---
+++ Bacis.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "THis is php\n";
+
+$x=70;
+$z=$x+30;
+$y= "The price is";
+echo"$y $z\n";
+// conditional structure
+if($x > $z){
+    echo"$x is the gretest\n";
+}
+else{
+    echo"$z is the gretest\n";
+}
+// loop
+for($a=0; $a<10; $a++){
+    echo"$a";
+}echo"\n";
+
+
+?>
